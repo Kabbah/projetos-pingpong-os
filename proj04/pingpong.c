535d4b53e84e0e6ea4c71a39c1ab32dc36e496d9
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2017-08-26 17:03:05 -0300

    Tentativa projeto 04.

---
+++ proj04/pingpong.c
@@ -0,0 +1,272 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pingpong.h"
+#include "queue.h"
+
+#define STACKSIZE 32768
+
+#define DEFAULT_PRIO 0
+#define MIN_PRIO -20
+#define MAX_PRIO 20
+#define ALPHA_PRIO 1
+
+/* Task main */
+task_t taskMain;
+
+/* Task em execução */
+task_t* taskExec;
+
+/* Task do dispatcher */
+task_t taskDisp;
+
+/* Ponteiro para uma task que deve ser liberada */
+task_t* freeTask;
+
+/* Fila de tasks prontas */
+task_t* readyQueue;
+
+/* Fila de tasks suspensas */
+task_t* suspendedQueue;
+
+/* ID da próxima task a ser criada */
+long nextid;
+
+/* Função a ser executada pela task do dispatcher*/
+void bodyDispatcher(void* arg);
+
+/* Função que retorna a próxima task a ser executada. */
+task_t* scheduler();
+
+void pingpong_init() {
+	/* Desativa o buffer de saída padrão */
+	setvbuf(stdout, 0, _IONBF, 0);
+
+	/* INICIA A TASK MAIN */
+	/* A task main não está na fila...? */
+	taskMain.next = NULL;
+	taskMain.prev = NULL;
+	taskMain.queue = NULL;
+	
+	/* Referência a si mesmo? */
+	taskMain.main = &taskMain;
+
+	/* A task main esta pronta. */
+	taskMain.estado = 'r';
+
+	/* A task main tem id 0. */
+	taskMain.tid = 0;
+
+	/* O id da próxima task a ser criada é 1. */
+	nextid = 1;
+
+	/* A primeira task em execução é a main. */
+	taskExec = &taskMain;
+
+	/* Nao ha nenhuma task para ser liberada. */
+	freeTask = NULL;
+
+	/* O contexto não precisa ser salvo agora, porque a primeira troca de contexto fará isso. */
+
+	/* INICIA A TASK SCHEDULER */
+	task_create(&taskDisp, &bodyDispatcher, NULL);
+	queue_remove((queue_t**) &readyQueue, (queue_t*) &taskDisp);
+	
+	#ifdef DEBUG
+	printf("PingPongOS iniciado.\n");
+	#endif
+}
+
+int task_create(task_t* task, void (*start_func)(void*), void* arg) {
+	char* stack;
+
+	/* Coloca referência para task main. */
+	task->main = &taskMain;
+
+	/* Inicializa o contexto. */
+	getcontext(&(task->context));
+
+	/* Aloca a pilha. */
+	stack = malloc(STACKSIZE);
+	if (stack == NULL) {
+		perror("Erro na criação da pilha: ");
+		return -1;
+	}
+
+	/* Seta a pilha do contexto. */
+	task->context.uc_stack.ss_sp = stack;
+	task->context.uc_stack.ss_size = STACKSIZE;
+	task->context.uc_stack.ss_flags = 0;
+
+	/* Não liga o contexto a outro. */
+	task->context.uc_link = NULL;
+
+	/* Cria o contexto com a função. */
+	makecontext(&(task->context), start_func, 1, arg);
+
+	/* Seta o id da task. */
+	task->tid = nextid;
+	nextid++;
+	
+	#ifdef DEBUG
+	printf("task_create: task %d criada.\n", task->tid);
+	#endif
+
+	/* Informações da fila. */
+	queue_append((queue_t**) &readyQueue, (queue_t*) task);
+	task->queue = &readyQueue;
+	task->estado = 'r';
+	task->prio = DEFAULT_PRIO;
+	task->dynPrio = task->prio;
+
+	return (task->tid);
+}
+
+void task_exit(int exitCode) {
+	#ifdef DEBUG
+	printf("task_exit: encerrando task %d.\n", taskExec->tid);
+	#endif
+	freeTask = taskExec;
+	
+	if (taskExec == &taskDisp) {
+		task_switch(&taskMain);
+	}
+	else {
+		task_switch(&taskDisp);
+	}
+}
+
+int task_switch(task_t *task) {
+	task_t* prevTask;
+
+	prevTask = taskExec;
+	taskExec = task;
+	
+	#ifdef DEBUG
+	printf("task_switch: trocando task %d -> %d.\n", prevTask->tid, task->tid);
+	#endif
+	
+	if (swapcontext(&(prevTask->context), &(task->context)) < 0) {
+		perror("Erro na troca de contexto: ");
+		return -1;
+	}
+
+	return 0;
+}
+
+int task_id() {
+	return (taskExec->tid);
+}
+
+void task_suspend(task_t *task, task_t **queue) {
+	/* Se task for nulo, considera a tarefa corrente. */
+	if (task == NULL) {
+		task = taskExec;
+	}
+
+	/* Se queue for nulo, não retira a tarefa. */
+	if (queue != NULL) {
+		#ifdef DEBUG
+		printf("task_suspend: queue é NULL, a tarefa %d não foi suspensa.\n", task->tid);
+		#endif
+		return;
+	}
+
+	/* Remove a task de sua fila atual e coloca-a na fila fornecida. */
+	queue_remove((queue_t**)(task->queue), (queue_t*)task);
+	queue_append((queue_t**)queue, (queue_t*)task);
+	task->queue = queue;
+	task->estado = 's';
+}
+
+void task_resume(task_t *task) {
+	/* Remove a task de sua fila atual e coloca-a na fila de tasks prontas. */
+	if (task->queue != NULL) {
+		queue_remove((queue_t**)(task->queue), (queue_t*)task);
+	}
+
+	queue_append((queue_t**)readyQueue, (queue_t*)task);
+	task->queue = &readyQueue;
+	task->estado = 'r';
+}
+
+void task_yield() {
+	/* Volta o controle para o dispatcher. */
+	task_switch(&taskDisp);
+}
+
+void task_setprio(task_t* task, int prio) {
+	if (task == NULL) {
+		task = taskExec;
+	}
+	if (prio <= MAX_PRIO && prio >= MIN_PRIO) {
+		task->prio = prio;
+	}
+}
+
+int task_getprio(task_t* task) {
+	if (task == NULL) {
+		task = taskExec;
+	}
+	return task->prio;
+}
+
+void bodyDispatcher(void* arg) {
+	while (queue_size((queue_t*) readyQueue)) {
+		task_t* next = scheduler();
+
+		if (next != 0) {
+			task_switch(next);
+
+			/* Libera a memoria da task, caso ela tenha dado exit. */
+			if (freeTask != NULL) {
+				free(freeTask->context.uc_stack.ss_sp);
+				freeTask = NULL;
+			}
+			else {
+				/* Recoloca a task no final da fila de prontas, caso ela tenha dado yield. */
+				queue_append((queue_t**)&readyQueue, (queue_t*)next);
+			}
+		}
+	}
+	task_exit(0);
+}
+
+task_t* scheduler() {
+	task_t* iterator;
+	task_t* nextTask;
+	int minDynPrio;
+
+	iterator = readyQueue;
+	nextTask = NULL;
+	minDynPrio = MAX_PRIO + 1;
+
+	/* Se a fila estiver vazia, retorna NULL. */
+	if (iterator == NULL) {
+		return NULL;
+	}
+
+	/* Busca a tarefa com menor dynPrio para executar. */
+	do {
+		if (iterator->dynPrio < minDynPrio) {
+			nextTask = iterator;
+			minDynPrio = iterator->dynPrio;
+		}
+
+		iterator = iterator->next;
+	} while (iterator != readyQueue);
+
+	/* Retira a tarefa da fila e reseta sua prioridade dinamica. */
+	queue_remove((queue_t**)&readyQueue, (queue_t*)nextTask);
+	nextTask->dynPrio = nextTask->prio;
+
+	/* Atualiza a dynprio das outras tarefas. */
+	iterator = readyQueue;
+	if (iterator != NULL) {
+		do {
+			iterator->dynPrio -= ALPHA_PRIO;
+			iterator = iterator->next;
+		} while (iterator != readyQueue);
+	}
+
+	return nextTask;
+}
---
+++ proj04/queue.c
@@ -0,0 +1,166 @@
+// =====================================
+// queue.c
+// Autor: Victor Barpp Gomes
+// Data de início: 17/08/2017
+// Data de término: 17/08/2017
+// Disciplina: Sistemas Operacionais
+// =====================================
+
+#include <stdio.h>
+#include "queue.h"
+
+void queue_append(queue_t** queue, queue_t* elem) {
+	// Se a fila não existe, aborta.
+	if (queue == NULL) {
+		#ifdef DEBUG
+		printf("Erro queue_append: A fila nao existe.\n");
+		#endif
+		return;
+	}
+
+	// Se o elemento não existe, aborta.
+	if (elem == NULL) {
+		#ifdef DEBUG
+		printf("Erro queue_append: O elemento nao existe.\n");
+		#endif
+		return;
+	}
+
+	// Se o elemento já está em uma fila, aborta.
+	if (elem->next != NULL || elem->prev != NULL) {
+		#ifdef DEBUG
+		printf("Erro queue_append: O elemento ja esta em uma fila.\n");
+		#endif
+		return;
+	}
+
+	// Se a fila está vazia, faz ela apontar para o elemento.
+	// Também ajusta os elementos próximo e anterior.
+	if (*queue == NULL) {
+		*queue = elem;
+		elem->next = elem;
+		elem->prev = elem;
+		return;
+	}
+
+	// Se a fila não está vazia, adiciona o elemento no final da fila.
+	// Isso é feito acessando o elemento anterior ao primeiro (que é o último, porque a fila é circular).
+	elem->prev = (*queue)->prev; // O anterior ao novo elemento é o antigo último elemento.
+	elem->next = (*queue);       // O próximo ao novo elemento é o primeiro elemento.
+	(*queue)->prev->next = elem; // O próximo ao antigo último elemento é o novo elemento.
+	(*queue)->prev = elem;       // O anterior ao primeiro elemento é o novo elemento.
+
+	return;
+}
+
+queue_t* queue_remove(queue_t** queue, queue_t* elem) {
+	// Se a fila não existe, aborta.
+	if (queue == NULL) {
+		#ifdef DEBUG
+		printf("Erro queue_remove: A fila nao existe.\n");
+		#endif
+		return NULL;
+	}
+
+	// Se o elemento não existe, aborta.
+	if (elem == NULL) {
+		#ifdef DEBUG
+		printf("Erro queue_remove: O elemento nao existe.\n");
+		#endif
+		return NULL;
+	}
+
+	queue_t* iterator = (*queue);
+
+	queue_t* nextElem;
+	queue_t* prevElem;
+
+	// Se a fila é vazia, retorna NULL.
+	if (*queue == NULL) {
+		#ifdef DEBUG
+		printf("Erro queue_remove: A fila esta vazia.\n");
+		#endif
+		return NULL;
+	}
+
+	// Verifica se o elemento realmente está na fila fornecida.
+	while (iterator != elem) {
+		iterator = iterator->next;
+
+		// Caso tenha chegado ao início da fila novamente, não remove o elemento.
+		if (iterator == (*queue)) {
+			#ifdef DEBUG
+			printf("Erro queue_remove: O elemento nao pertence a fila indicada.\n");
+			#endif
+			return NULL;
+		}
+	}
+
+	// Se a fila tem só um elemento, deixa a fila vazia.
+	// Também ajusta os elementos próximo e anterior.
+	if (elem->next == elem && elem->prev == elem) {
+		elem->next = NULL;
+		elem->prev = NULL;
+		(*queue) = NULL;
+
+		return elem;
+	}
+
+	// Se a fila tem mais de um elemento, retira o elemento e "gruda" os pedaços.
+	// Se o elemento for o primeiro, reajusta o ponteiro da fila para o segundo.
+	if ((*queue) == elem) {
+		(*queue) = elem->next;
+	}
+	nextElem = elem->next;
+	prevElem = elem->prev;
+
+	nextElem->prev = prevElem;
+	prevElem->next = nextElem;
+
+	elem->next = NULL;
+	elem->prev = NULL;
+
+	return elem;
+}
+
+int queue_size(queue_t* queue) {
+	int i = 0;
+	queue_t* iterator;
+
+	// Se a fila é nula, então tem zero elementos.
+	if (queue == NULL) {
+		return 0;
+	}
+
+	iterator = queue->next;
+
+	for (i = 1; iterator != queue; iterator = iterator->next, i++);
+
+	return i;
+}
+
+void queue_print(char* name, queue_t* queue, void print_elem(void*)) {
+	queue_t* iterator = queue;
+
+	printf(name);
+	printf("[");
+
+	// Se a fila é nula, então aborta.
+	if (queue == NULL) {
+		printf("]\n");
+		return;
+	}
+
+	// Caso externo (só para não printar o primeiro espaço a esquerda)
+	print_elem(iterator);
+	iterator = iterator->next;
+
+	while(iterator != queue) {
+		printf(" ");
+		print_elem(iterator);
+		iterator = iterator->next;
+	}
+
+	printf("]\n");
+	return;
+}
